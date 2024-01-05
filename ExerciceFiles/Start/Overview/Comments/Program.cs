a9149abd33dddc839c6fc303e2792d42c5cad717
Moussa Haroune <haroune.hmah@gmail.com>
2024-01-05 18:24:28 +0100

    comments XML use in c# .net

--- ExerciceFiles/Start/Overview/Comments/Program.cs
+++ ExerciceFiles/Start/Overview/Comments/Program.cs
@@ -6,10 +6,19 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// this is the main sample application function
+        /// </summary>
+        /// /// <param name='args'> An array of strinf arguements from command line </param>
+        /// <returns>
+        /// No return value
+        /// </returns>
         static void Main(string[] args)
         {
-
+            // Single line comments start with twp slashes
             Console.WriteLine("Hello World!");
+            /* Multiple line comments start with a slash and a star and can continuefor several lines
+            until a closinf star and slash are encountered */
 
         }
     }
