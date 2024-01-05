5c116daeb0acfa9bed775a551f991d79979e5f86
Moussa Haroune <haroune.hmah@gmail.com>
2024-01-05 19:37:59 +0100

    conditionals with switch case in c#

--- ExerciceFiles/Start/Program Flow/Conditional-switch/Program.cs
+++ ExerciceFiles/Start/Program Flow/Conditional-switch/Program.cs
@@ -6,9 +6,27 @@ namespace ConditionalOps
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 60;
 
             // TODO: The switch statement
+            switch (theVal) {
+                case 50: 
+                Console.WriteLine("theVal is 50");
+                break;
+                case 51: 
+                Console.WriteLine("theVal is 51");
+                break;
+
+                case 52: 
+                case 53: 
+                case 54: 
+                Console.WriteLine("theVal is betwen 52 and 54");
+                break;
+            default:
+                Console.WriteLine("theVal is somthing else");
+                break;
+
+            }
 
         }
     }
