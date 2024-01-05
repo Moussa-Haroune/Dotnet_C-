82365dcb06392748ca4e419dfad89f41760eb39f
Moussa Haroune <haroune.hmah@gmail.com>
2024-01-05 17:25:41 +0100

    first code Hello world, in c#

---
+++ ExerciceFiles/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Whate is your name");
+            string str = Console.ReadLine();
+            Console.WriteLine("Hello, Welcome " + str + " !");
+        }
+    }
+}
